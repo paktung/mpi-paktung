a2efada80d5d8592de8e0bf4d0fe6d9810c849ab
Untung Supriyono <itsmartz79@gmail.com>
2026-05-12 20:12:19 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://wfygasgikgclaqzmwjnh.supabase.co";
+const SUPABASE_KEY = "sb_publishable_qBEZCpWjHgZyJLn59hdyPA_zRGLWinV";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
